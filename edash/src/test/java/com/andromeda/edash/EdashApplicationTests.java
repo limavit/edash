e44c7e630a248a7536bf32fc0068073fc1f082fb
limavit <limavit@gmail.com>
2024-09-18 11:22:59 -0300

    commit inicial

---
+++ edash/src/test/java/com/andromeda/edash/EdashApplicationTests.java
@@ -0,0 +1,13 @@
+package com.andromeda.edash;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EdashApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
